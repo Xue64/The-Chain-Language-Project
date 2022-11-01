c8b7ea69c39371f47b72c8944872c6793eee333b
Xue64 <rufelleemmanuelpactol7@gmail.com>
2022-11-02 01:37:51 +0800

    relegated internet check to $update only

--- vercontrol.h
+++ vercontrol.h
@@ -92,6 +92,9 @@ namespace rsasm {
         }
 
         void install_update (){
+            std::cout << "Checking for internet availability, this may take a while...\n";
+            connected = system("ping google.com > null");
+            connected = !connected;
             if(!get_internet()){
                 std::cout << "Cannot install update, please check your internet privileges.\n";
                 return;
@@ -126,35 +129,32 @@ namespace rsasm {
         const char * curr_version = CURR_VER;
 
         std::fstream version_control;
-        std::cout << "Checking for internet availablity, this may take a while...\n";
-        connected = system("ping google.com > null");
-        connected = !connected;
+
         shell::system_no_output("cd RSASM-Files && git pull CubeClub-RSASM-Ver-Checker > nul");
         version_control.open("RSASM-Files\\updater.txt", std::ios::in);
-        if (!connected){
-            std::cout << "\nNo internet connection found, unable to check for updates.";
-        } else {
-			
-			
-            if (!version_control){
-                std::cout << "\n\nUpon invoking of system file integrity check, found one (1) vulnerability. Version control dependency not found.\nWill be unable to update this installation of RS ASM.\n";
 
+			
+			
+        if (!version_control){
+            std::cout << "\nUpon invoking of system file integrity check, found one (1) vulnerability. Version control dependency not found.\nWill be unable to update this installation of RS ASM.\n";
+
+
+        } else {
+            std::string remote_ver;
+            std::getline(version_control, remote_ver);
+            if (!strcmp(curr_version, remote_ver.c_str())){
 
             } else {
-                std::string remote_ver;
-                std::getline(version_control, remote_ver);
-                if (!strcmp(curr_version, remote_ver.c_str())){
 
-                } else {
-
-                    std::cout << "\n\nA new version is available! Please go to the update settings using the \"$update\" command.\n";
-                }
+                std::cout << "\n\nA new version is available! Please go to the update settings using the \"$update\" command.\n";
             }
+
         }
 
     }
 
     std::string get_version (){
+
         std::string return_value(CURR_VER);
         return return_value;
     }
